7a5f5972c5591d9d120ff9406b6d6a39bd34b6c5
Pientaszek94 <pawel.pienta@gmail.com>
2024-03-24 22:10:15 +0100

    second commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import styles from "../styles/footer.module.scss"
 function Footer() {
+  const food="https://www.wallpapers.net/free-download-assorted-food-hd-wallpaper-for-desktop-mobiles/download/5120x2160.jpg"
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>
+      <div style={{
+              backgroundImage:`url(${food})`,
+              backgroundPosition:"center",
+              backgroundSize:"cover"
+              }}></div>
+      <h1>
+          PWL Frontdev
+      </h1>
+      <h5>
+          Made with curiousity and a itsy bitsy fun
+      </h5>
+    </div>
   )
 }
 
